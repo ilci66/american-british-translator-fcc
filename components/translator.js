5cebcd5f79bf9a82ed9b67e009ed511ab17281ef
ilci66 <77514750+ilci66@users.noreply.github.com>
2021-06-09 13:03:15 +0200

    fix replace logic

--- components/translator.js
+++ components/translator.js
@@ -14,34 +14,138 @@ const reverser = (object) => {
   return result;
 }
 class Translator {
-  
-  americanToBritish(stringOfWords){
-
-    let reversedBritishOnly = reverser(britishOnly)
-
-    let ameToBriAll = {...americanToBritishTitles,...americanToBritishSpelling,...americanOnly,...reversedBritishOnly}
-
-    let split = stringOfWords.split(' ')
-    console.log(split)
-    for(let i = 0; i < split.length; i++){
-      if(ameToBriAll[split[i]]){
-        console.log('yep')
-      }
+  britishToAmerican(text){
+    const dictionary = {...britishOnly,...reverser(americanToBritishSpelling)}
+    const titles = reverser(americanToBritishTitles)
+    const timeRegex = /([1-9]|1[012]).[0-5][0-9]/g; 
+    const locale = "translateToAmerican"
+    const translated = this.translate(text, dictionary, titles, timeRegex, locale)
+    //if matchesmap is empty returns null, therefore translated is falsy
+    if(!translated){
+      return text
     }
-    //this returns an array of key value pairs Object.fromEntries fixes it 
-    // console.log(Object.entries(ameToBriAll).filter(([k, v]) => k.includes(" ")))
-
+    return translated
+    
+  }
+ 
+  americanToBritish(text){
+    const dictionary = {...americanOnly,...americanToBritishSpelling}
+    const titles = americanToBritishTitles
+    const timeRegex = /([1-9]|1[012]):[0-5][0-9]/g; 
+    const locale = "translateToBritish"
+    const translated = this.translate(text, dictionary, titles, timeRegex, locale)
+    //if matchesmap is empty returns null, therefore translated is falsy
+    if(!translated){
+      return text
+    }
+    return translated
+    
   }
 
-  britishToAmerican(stringOfWords){
-    let reversedAmericanOnly = reverser(americanOnly)
-    let britishToAmericanTitles = reverser(americanToBritishTitles)
-    let britishToAmericanSpelling = reverser(americanToBritishSpelling)
+  translate(text, dictionary, titles, timeRegex, locale){
+    const givenText = text
+    const lowerText = givenText.toLowerCase();
+    const matchesMap = {};
 
-    let briToAmeAll = {...reversedAmericanOnly, ...britishToAmericanTitles, ...britishToAmericanSpelling, ...britishOnly}
 
-    console.log(briToAmeAll)
+    // Search for titles/honorifics and add'em to the matchesMap object
+    Object.entries(titles).map(([k, v]) => {
+      if (lowerText.includes(k)) {
+        matchesMap[k] = v[0].toUpperCase() + v.slice(1);
+        // matchesMap[k] = v.charAt(0).toUpperCase() + v.slice(1);
+      }
+    });
+    //check for words with spcaes first otherwise some translations come out wrong
+    const wordsWithSpace = Object.fromEntries(
+      Object.entries(dictionary).filter(([k, v]) => k.includes(" "))
+    );
+
+    Object.entries(wordsWithSpace).map(([k, v]) => {
+      if (lowerText.includes(k)) {
+        matchesMap[k] = v;
+      }
+    });
+
+    //now look for individual words 
+    lowerText
+      .match(/(\w+([-'])(\w+)?['-]?(\w+))|\w+/g)
+      .map((word) => {
+        if (dictionary[word]) return (matchesMap[word] = dictionary[word]);
+      });
+    
+    // console.log(matchesMap)
+
+    //match the given times too, sorta annoying 
+    const matchTimes = lowerText.match(timeRegex)
+    if (matchTimes) {
+      matchTimes.map((e) => {
+        if (locale === "translateToBritish") {
+          return (matchesMap[e] = e.replace(":", "."));
+        }
+        return (matchesMap[e] = e.replace(".", ":"));
+      });
+    }
+
+    console.log(matchesMap)
+
+    
+    //handle no matches situation here, no need to go further
+    if(Object.keys(matchesMap).length == 0) return null;
+    
+    const translationNoHighlight = this.replaceAll(text, matchesMap);
+    const translationHighlighted = this.replaceAllHighlighted(text, matchesMap)
+    
+    //don't forget to return 
+    return [translationNoHighlight, translationHighlighted];
   }
+
+  replaceAll(text, matchesMap){
+    // make a regex out of the that matched in order to replace 
+    const re = new RegExp(Object.keys(matchesMap).join("|"), "gi");
+
+    return text.replace(re, (word) => matchesMap[word.toLowerCase()]);
+  }
+
+  replaceAllHighlighted(text, matchesMap){
+    const re = new RegExp(Object.keys(matchesMap).join("|"), "gi");
+
+    return text.replace(re, (word) => {
+       return `<span class="highlight">${matchesMap[word.toLowerCase()]}</span>`
+    })
+  }
+
+
+
+
+
+
+  // americanToBritish(stringOfWords){
+
+  //   let reversedBritishOnly = reverser(britishOnly)
+
+  //   let ameToBriAll = {...americanToBritishTitles,...americanToBritishSpelling,...americanOnly,...reversedBritishOnly}
+
+  //   let split = stringOfWords.split(' ')
+  //   console.log(split)
+  //   for(let i = 0; i < split.length; i++){
+  //     if(ameToBriAll[split[i]]){
+  //       console.log('yep')
+  //     }
+  //   }
+  //   //this returns an array of key value pairs Object.fromEntries fixes it 
+  //   // console.log(Object.entries(ameToBriAll).filter(([k, v]) => k.includes(" ")))
+
+  // }
+
+  // britishToAmerican(stringOfWords){
+  //   let reversedAmericanOnly = reverser(americanOnly)
+  //   let britishToAmericanTitles = reverser(americanToBritishTitles)
+  //   let britishToAmericanSpelling = reverser(americanToBritishSpelling)
+
+  //   let briToAmeAll = {...reversedAmericanOnly, ...britishToAmericanTitles, ...britishToAmericanSpelling, ...britishOnly}
+
+  //   console.log(briToAmeAll)
+  // }
 
 
 
--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,33 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
+      const { locale } = req.body;
+      const { text } = req.body
+      let translation;
+      if(text == ""){
+        return res.json({"error":"No text to translate"});
+      }
+      if(!locale || text == undefined){
+        return res.json({ error: 'Required field(s) missing' });
+      }
+      if(locale != "american-to-british" && locale != "british-to-american"){
+        return res.json({ error: 'Invalid value for locale field' });
+      }
+      if(locale == "american-to-british"){
+        translation = translator.americanToBritish(text)
+      }else if(locale == "british-to-american"){
+        translation = translator.britishToAmerican(text)
+      }
 
-      translator.americanToBritish('mr. colorful')
-    });
+      if(translation == text){
+        return res.json({text: text, translation:"Everything looks good to me!"})
+      }else if(translation != text){
+        //bring the highlighted one 
+        return res.json({text, translation: translation[1]})
+      }
+    })
+      // console.log(req.body.locale)
+      // console.log(req.body.text)
+      // translator.britishToAmerican('dr colourful flat 10.10')
+  
 };
